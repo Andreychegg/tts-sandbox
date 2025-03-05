e0b0c6974bfbfdfcafb8da19670716ae096daf89
Andreychegg <andrey.sukhov2014@yandex.ru>
2025-03-05 08:06:25 +0500

    Разделение кода на модули

--- app.py
+++ app.py
@@ -1,28 +1,8 @@
 from flask import Flask, request, jsonify, send_from_directory
+from speech.synthesizer import SpeechSynthesizer
 import os
-import pyttsx3
-from gtts import gTTS
 
 app = Flask(__name__)
-
-class SpeechSynthesizer:
-    def __init__(self):
-        self.engine = pyttsx3.init()
-        self.engine.setProperty("rate", 150)
-        self.engine.setProperty("volume", 1.0)
-
-    def synthesize_with_gtts(self, text): # Синтез gTTS
-        output_file = "static/output_gtts.mp3"
-        tts = gTTS(text=text, lang="ru")
-        tts.save(output_file)
-        return output_file
-
-    def synthesize_with_pyttsx3(self, text): # Синтез pyttsx3
-        output_file = "static/output_pyttsx3.wav"
-        self.engine.save_to_file(text, output_file)
-        self.engine.runAndWait()
-        return output_file
-
 synthesizer = SpeechSynthesizer()
 
 @app.route("/")

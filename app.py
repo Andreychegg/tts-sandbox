6b6b0b610e2a547f20547ee0fa825670a1047995
Andreychegg <andrey.sukhov2014@yandex.ru>
2025-03-17 16:20:50 +0500

    Добавлен Edge-TTS

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, send_from_directory
 from speech.synthesizer import SpeechSynthesizer
 import os
 import time
+import asyncio
 
 app = Flask(__name__)
 synthesizer = SpeechSynthesizer()
@@ -16,14 +17,20 @@ def synthesize():
     text = data.get("text", "")
     method = data.get("method", "gtts")
     format = data.get("format", "mp3")
+    language = data.get("language", "ru") if method == "gtts" else "ru"
+    voice = data.get("voice", "ru-RU-SvetlanaNeural")
 
     if not text:
         return jsonify({"error": "Текст не должен быть пустым"}), 400
 
     if method == "gtts":
-        file_path = synthesizer.synthesize_with_gtts(text, format)
-    else:
+        file_path = synthesizer.synthesize_with_gtts(text, format, language)
+    elif method == "pyttsx3":
         file_path = synthesizer.synthesize_with_pyttsx3(text, format)
+    elif method == "edgetts":
+        file_path = asyncio.run(synthesizer.synthesize_with_edgetts(text, voice, format))
+    else:
+        return jsonify({"error": "Неподдерживаемый метод синтеза"}), 400
 
     timestamp = int(time.time())
     audio_url = f"/{file_path}?t={timestamp}"
--- speech/synthesizer.py
+++ speech/synthesizer.py
@@ -1,5 +1,7 @@
+import edge_tts
 import pyttsx3
 from gtts import gTTS
+
 
 class SpeechSynthesizer:
     def __init__(self):
@@ -7,14 +9,20 @@ class SpeechSynthesizer:
         self.engine.setProperty("rate", 150)
         self.engine.setProperty("volume", 1.0)
 
-    def synthesize_with_gtts(self, text, format="mp3"): # Синтез gTTS
+    def synthesize_with_gtts(self, text, format="mp3", lang="ru"):
         output_file = f"static/output_gtts.{format}"
-        tts = gTTS(text=text, lang="ru")
+        tts = gTTS(text=text, lang=lang)
         tts.save(output_file)
         return output_file
 
-    def synthesize_with_pyttsx3(self, text, format="wav"): # Синтез pyttsx3
+    def synthesize_with_pyttsx3(self, text, format="wav"):
         output_file = f"static/output_pyttsx3.{format}"
         self.engine.save_to_file(text, output_file)
         self.engine.runAndWait()
+        return output_file
+
+    async def synthesize_with_edgetts(self, text, voice="ru-RU-SvetlanaNeural", format="mp3"):
+        output_file = f"static/output_edgetts.{format}"
+        communicate = edge_tts.Communicate(text, voice)
+        await communicate.save(output_file)
         return output_file
